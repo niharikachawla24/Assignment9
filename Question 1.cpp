8f71ef8848c57f06045cefe24999c25bdff79a5b
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-29 00:25:09 +0530

    Update Question 1.cpp

--- Question 1.cpp
+++ Question 1.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int main() {
     int V,E,u,v;
-    cin >> V >> E;
+    cin>>V>>E;
 
-    int g[100][100] = {0};
-    while (E--) cin >> u >> v, g[u][v] = g[v][u] = 1;
+    int g[100][100]={0};
+    while(E--) cin>>u>>v, g[u][v]=g[v][u]=1;
 
-    int vis[100] = {0}, q[100], f = 0, r = 0;
-    q[r++] = 0; 
-    vis[0] = 1;
+    int vis[100]={0}, q[100], f=0, r=0;
+    q[r++]=0; vis[0]=1;
 
-    while (f < r) {
-        int x = q[f++];
-        cout << x << " ";
-        for (int i = 0; i < V; i++)
-            if (g[x][i] && !vis[i])
-                vis[i] = 1, q[r++] = i;
+    while(f<r){
+        int x=q[f++];
+        cout<<x<<" ";
+        for(int i=0;i<V;i++)
+            if(g[x][i] && !vis[i])
+                vis[i]=1, q[r++]=i;
     }
 }
+
