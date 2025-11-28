f3e5a4b6d413cb52a9f9ec16a89953e49b53eb69
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-29 00:26:21 +0530

    Update Question 4.cpp

--- Question 4.cpp
+++ Question 4.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     int V,E,u,v,w;
-    cin >> V >> E;
+    cin>>V>>E;
 
-    int g[100][100];
-    for(int i=0;i<V;i++)
-        for(int j=0;j<V;j++)
-            g[i][j] = 0;
-
-    while(E--) cin >> u >> v >> w, g[u][v] = w;
+    int g[100][100]={0};
+    while(E--) cin>>u>>v>>w, g[u][v]=w;
 
     int dist[100], used[100]={0};
-    for(int i=0;i<V;i++) dist[i] = 1e9;
-    dist[0] = 0;
+    for(int i=0;i<V;i++) dist[i]=100000000;
+    dist[0]=0;
 
     for(int i=0;i<V;i++){
-        int mn = 1e9, x = -1;
+        int x=-1, mn=100000000;
         for(int j=0;j<V;j++)
-            if(!used[j] && dist[j] < mn)
-                mn = dist[j], x = j;
+            if(!used[j] && dist[j]<mn)
+                mn=dist[j], x=j;
 
-        used[x] = 1;
+        used[x]=1;
 
         for(int j=0;j<V;j++)
-            if(g[x][j] && dist[x] + g[x][j] < dist[j])
+            if(g[x][j] && dist[x]+g[x][j] < dist[j])
                 dist[j] = dist[x] + g[x][j];
     }
 
-    for(int i=0;i<V;i++) cout << dist[i] << " ";
+    for(int i=0;i<V;i++) cout<<dist[i]<<" ";
 }
