0a46612a1fc464fdafd35732898edfdb50a03b02
niharikachawla24 <nchawla_be24@thapar.edu>
2025-11-29 00:25:30 +0530

    Update Question 2.cpp

--- Question 2.cpp
+++ Question 2.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int V, g[100][100], vis[100];
 
-void dfs(int x) {
-    vis[x] = 1;
-    cout << x << " ";
-    for (int i = 0; i < V; i++)
-        if (g[x][i] && !vis[i])
+void dfs(int x){
+    vis[x]=1;
+    cout<<x<<" ";
+    for(int i=0;i<V;i++)
+        if(g[x][i] && !vis[i])
             dfs(i);
 }
 
-int main() {
+int main(){
     int E,u,v;
-    cin >> V >> E;
-    while (E--) cin >> u >> v, g[u][v] = g[v][u] = 1;
+    cin>>V>>E;
+    while(E--) cin>>u>>v, g[u][v]=g[v][u]=1;
     dfs(0);
 }
